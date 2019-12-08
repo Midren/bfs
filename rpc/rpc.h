ddcddb7c504e93e398ad9fc2cc9157d4fea3bd39
Roman Milishchuk <milromchuk@gmail.com>
2019-12-08 22:24:32 +0200

    Added crypto++ and json support

--- rpc/rpc.h
+++ rpc/rpc.h
@@ -4,6 +4,22 @@
 #include <string>
 #include <vector>
 
+#include <boost/property_tree/ptree.hpp>
+
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/keccak.h>
+
+enum eth_method {
+    call,
+    send
+};
+
+typedef std::array<byte, 4> method_id;
+
+method_id get_method_id(std::string msg);
+
+std::vector<byte> encode_string(std::string);
+
 class rpc {
 public:
 
@@ -32,7 +48,24 @@ public:
     int remove_dir(std::string path);
 
 private:
+    template<typename... Args>
+    boost::property_tree::ptree form_json(eth_method method, Args... args);
 };
+
+template<typename... Args>
+boost::property_tree::ptree rpc::form_json(eth_method method, Args... args) {
+    boost::property_tree::ptree pt{};
+    pt.put("jsonrpc", 2.0);
+    switch (method) {
+        case eth_method::call:
+            pt.put("method", "eth_call");
+        case eth_method::send:
+            pt.put("method", "eth_sendTransactioon");
+    }
+
+
+    return pt;
+}
 
 
 #endif //RPC_RPC_H
