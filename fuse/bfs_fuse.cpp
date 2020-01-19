2fcbb9bdbed4053519862cb5d11f77c627f29255
Hermann Yavorskyi <yavorskyi@ucu.edu.ua>
2020-01-19 15:54:09 +0200

    Updated FUSE operations
    All implemented on RPC and on the Solidity side calls implemented in FUSE. Mounting TBD.

--- fuse/bfs_fuse.cpp
+++ fuse/bfs_fuse.cpp
@@ -1,14 +1,13 @@
 #include "bfs_fuse.h"
-#include <fuse.h>
 #include "../rpc/rpc.h"
 
 static Rpc RPC{"0x00c469eee8b9bc1a331070be0e5814a0bc6f902e", "0x3fDCC710BA867541a2031c25bed49fdA04C2D1b7"};
 
-int bfs_read(const char *path, char *buf, size_t size, off_t offset) {
+int bfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *) {
     return RPC.read_file(path, reinterpret_cast<uint8_t *>(buf), size, offset);
 }
 
-int bfs_write(const char *path, const char *data, size_t size, off_t offset) {
+int bfs_write(const char *path, const char *data, size_t size, off_t offset, struct fuse_file_info *) {
     return RPC.write_file(path, reinterpret_cast<const uint8_t *>(data), size); // TODO: Add offset
 }
 
@@ -20,21 +19,65 @@ int bfs_mkdir(const char *path, mode_t) {
     return RPC.create_dir(path);
 }
 
+int bfs_opendir(const char *path, struct fuse_file_info *) {
+    return 0; // TODO: Check directory existence and permissions
+}
+
+int bfs_open(const char *path, struct fuse_file_info *fileInfo) {
+    // TODO: if not exists create else return 0
+    return 0; // TODO: Check if file exists, permissions
+}
+
+int bfs_readdir(const char *path, void *buff, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fileInfo) {
+    std::vector<std::string> dirEntries = RPC.list_dir(path);
+    for (const auto &entry:dirEntries) {
+        if (filler(buff, entry.data(), nullptr, 0) != 0)
+            return -EXIT_FAILURE;
+    }
+    return 0;
+}
+
+int bfs_getattr(const char *path, struct stat *st) {
+    // TODO: return RPC.stat(path,st)
+}
+
+void *bfs_init(struct fuse_conn_info *conn) {
+    return 0;
+}
+
+void bfs_destroy(void *) {
+}
+
+int bfs_unlink(const char *path) {
+    return RPC.remove_file(path);
+}
+
+int bfs_rmdir(const char *path) {
+    return RPC.remove_dir(path);
+}
+
 
 struct fuse_operations bfs_operations = {
-        .open = [](const char *, int) { return 0; }, // We don`t need to open a file in BFS, TODO: Check if file exists
+        .getattr = bfs_getattr,
         .mknod = bfs_create_file,
+        .mkdir = bfs_mkdir,
+        .unlink = bfs_unlink,
+        .rmdir = bfs_rmdir,
+        .open = bfs_open, // We don`t need to open a file in BFS
         .read = bfs_read,
         .write = bfs_write,
-        .mkdir = bfs_mkdir
+        .opendir = bfs_opendir,
+        .readdir = bfs_readdir,
+        .init = bfs_init,
+        .destroy = bfs_destroy
 };
 
 int main(int argc, char *argv[]) {
-    if ((getuid() == 0) || (geteuid() == 0)) {
-        std::cout << "[WARNING]:\t Running as root!" << std::endl;
-    }
-    if (argc != 3) {
-        throw std::invalid_argument("Invalid arguments.\n\tBFS [root_directory] [mount_point]");
-    }
-    return fuse_main(argc, argv, &bfs_operations);
+//    if ((getuid() == 0) || (geteuid() == 0)) {
+//        std::cout << "[WARNING]:\t Running as root!" << std::endl;
+//    }
+//    if (argc != 3) {
+//        throw std::invalid_argument("Invalid arguments.\n\tBFS [root_directory] [mount_point]");
+//    }
+//    return fuse_main(argc, argv, &bfs_operations);
 }
--- fuse/bfs_fuse.h
+++ fuse/bfs_fuse.h
@@ -3,12 +3,32 @@
 
 #include <cstdlib>
 
-int bfs_read(const char *path, char *buf, size_t size, off_t offset);
+#define FUSE_USE_VERSION 26
 
-int bfs_write(const char *path, const char *data, size_t size, off_t offset);
+#include <fuse.h>
+#include <sys/stat.h>
+
+
+int bfs_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *);
+
+int bfs_write(const char *path, const char *data, size_t size, off_t offset,struct fuse_file_info *);
 
 int bfs_create_file(const char *path, mode_t mode, dev_t dev);
 
 int bfs_mkdir(const char *path, mode_t);
 
+int bfs_opendir(const char *path, struct fuse_file_info *);
+
+int bfs_open(const char *path, struct fuse_file_info *fileInfo);
+
+int bfs_readdir(const char *path, void *buff, fuse_fill_dir_t filler, off_t offset,
+                struct fuse_file_info *fileInfo);
+
+int bfs_getattr(const char *path, struct stat *st);
+
+void *bfs_init(struct fuse_conn_info *conn);
+
+void bfs_destroy(void *);
+int bfs_unlink(const char *path);
+int bfs_rmdir(const char* path);
 #endif //RPC_BFS_FUSE_H
